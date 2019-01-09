7edce786fc641f2f613476bcde72ba6ad641a305
FANG philippe <fang@informatique.univ-paris-diderot.fr>
2019-01-09 18:33:29 +0100

    FIN

--- arbre_bsp.ml
+++ arbre_bsp.ml
@@ -1,22 +1,22 @@
 open Graphics
-
+   
 type label = {
-  coord : int ;
-  colored : bool;
-}
-
+    coord : int ;
+    colored : bool;
+  }
+           
 type bsp =
-R of color option
-  | L of label * bsp * bsp
-
+  R of color option
+| L of label * bsp * bsp
+     
 let size = 20 (* Dimension minimale d'un rectangle *)
-
+         
 let purple_line = rgb 200 0 200
 let red_line = rgb 255 0 0
 let blue_line = rgb 0 0 255
 let red_rect = rgb 150 0 0
 let blue_rect = rgb 0 0 150
-
+              
 (* renvoie un entier aleatoire entre mn et mx *)
 let random_int mn mx=
   Random.self_init();
@@ -26,20 +26,20 @@ let random_int mn mx=
 let random_bsp width height depth_max =
   assert(depth_max>=0);
   (* les x et y sont les bornes de generation des coordonnées *)
-let rec random_noeud depth parite xmin xmax ymin ymax =
-  if depth=0 || xmin>xmax || ymin>ymax then if (Random.bool()) then
-      R(Some blue_rect) else R(Some red_rect)
-  else if parite then
-    let v = (random_int xmin xmax) in
-    L({coord=v;colored=(not (Random.bool()) || Random.bool())},
-      random_noeud (Random.int depth) (not parite) xmin (v-size) ymin ymax,
-      random_noeud (Random.int depth) (not parite) (v+size) xmax ymin ymax)
-  else
-    let v = (random_int ymin ymax) in
-    L({coord=v;colored=(not (Random.bool()) || Random.bool())},
-      random_noeud (Random.int depth) (not parite) xmin xmax ymin (v-size),
-      random_noeud (Random.int depth) (not parite) xmin xmax (v+size) ymax)
-in random_noeud depth_max true size (width-size) size (height-size)
+  let rec random_noeud depth parite xmin xmax ymin ymax =
+    if depth=0 || xmin>xmax || ymin>ymax then
+      if (Random.bool()) then R(Some blue_rect) else R(Some red_rect)
+    else if parite then
+      let v = (random_int xmin xmax) in
+      L({coord=v;colored=(not (Random.bool()) || Random.bool())},
+        random_noeud (Random.int depth) (not parite) xmin (v-size) ymin ymax,
+        random_noeud (Random.int depth) (not parite) (v+size) xmax ymin ymax)
+    else
+      let v = (random_int ymin ymax) in
+      L({coord=v;colored=(not (Random.bool()) || Random.bool())},
+        random_noeud (Random.int depth) (not parite) xmin xmax ymin (v-size),
+        random_noeud (Random.int depth) (not parite) xmin xmax (v+size) ymax)
+  in random_noeud depth_max true size (width-size) size (height-size)
 ;;
 
 let rectangles_from_line bsp parite =
@@ -49,31 +49,32 @@ let rectangles_from_line bsp parite =
     match bsp_tmp with
       R(a) -> [(a,father,fleaf)]
     | L(l,g,d) ->
-      if parite_tmp then
-        if rleaf then (aux d (not parite_tmp) rleaf l.coord false)
-        else (aux g (not parite_tmp) rleaf l.coord true)
-      else (aux g (not parite_tmp) rleaf l.coord true)
-           @(aux d (not parite_tmp) rleaf l.coord false)
+       if parite_tmp then
+         if rleaf then (aux d (not parite_tmp) rleaf l.coord false)
+         else (aux g (not parite_tmp) rleaf l.coord true)
+       else (aux g (not parite_tmp) rleaf l.coord true)
+            @(aux d (not parite_tmp) rleaf l.coord false)
   in match bsp with
-    L(l,g,d) ->
-    if parite then (aux g (not parite) true l.coord true)
-                   @(aux d (not parite) false l.coord false)
-    else  (aux g parite true l.coord true)@(aux d parite false l.coord false)
-  | _ -> failwith "L type required"
-
+       L(l,g,d) ->
+        if parite then (aux g (not parite) true l.coord true)
+                       @(aux d (not parite) false l.coord false)
+        else  (aux g parite true l.coord true)@(aux d parite false l.coord false)
+     | _ -> failwith "L type required"
+          
 let line_color line parite =
   (* les count determineront la couleur de la ligne *)
   let rec aux count_blue count_red l =
     match l with
       ((Some a),_,_)::v ->
-    if a = blue_rect then aux (count_blue+1) count_red v
-    else aux count_blue (count_red+1) v
+       if a = blue_rect then aux (count_blue+1) count_red v
+       else aux count_blue (count_red+1) v
     | _ ->
-      if count_blue>count_red then blue_line
-      else if count_blue<count_red then red_line
-      else purple_line
+       if count_blue>count_red then blue_line
+       else if count_blue<count_red then red_line
+       else purple_line
   in match line with
-    L(v,g,d) ->
-    if not (v.colored) then black
-    else let rectangles = (rectangles_from_line line parite) in aux 0 0 rectangles
-  | _ -> failwith "L type required"
+       L(v,g,d) ->
+        if not (v.colored) then black
+        else let rectangles = (rectangles_from_line line parite) in aux 0 0 rectangles
+     | _ -> failwith "L type required"
+              
--- mondrian.ml
+++ mondrian.ml
@@ -1,7 +1,7 @@
 open Arbre_bsp
 open Sat_solver
 open Graphics
-
+   
 module Variables = struct
   type t = int * bool
   let compare x y =
@@ -12,33 +12,33 @@ module Variables = struct
     else if x2=true then 1
     else -1
 end
-
+                 
 module Sat = Sat_solver.Make(Variables)
-
+           
 let config_initial config_final =
   let rec aux bsp =
-  match bsp with
-    R(a) -> R(None)
-  | L(l,g,d) -> L(l,aux g,aux d)
+    match bsp with
+      R(a) -> R(None)
+    | L(l,g,d) -> L(l,aux g,aux d)
   in aux config_final
 ;;
 
 (* verifie si tous les rectangles sont coloriés *)
 let rec is_full bsp =
-    match bsp with
-      L(l,g,d) -> (is_full g) && (is_full d)
-    | R(None) -> false
-    | _ -> true
+  match bsp with
+    L(l,g,d) -> (is_full g) && (is_full d)
+  | R(None) -> false
+  | _ -> true
 ;;
 
 let check_current bsp1 bsp2 =
   let rec aux tmp1 tmp2 parite=
     match tmp1,tmp2 with
       L(l,g,d),L(l',g',d') ->
-      let couleur1 = line_color tmp1 parite in
-      let couleur2 = line_color tmp2 parite in
-      if couleur1<>couleur2 && l.colored=true then false
-      else (aux g g' (not parite)) && (aux d d' (not parite))
+       let couleur1 = line_color tmp1 parite in
+       let couleur2 = line_color tmp2 parite in
+       if couleur1<>couleur2 && l.colored=true then false
+       else (aux g g' (not parite)) && (aux d d' (not parite))
     | _ -> true
   in aux bsp1 bsp2 true
 ;;
@@ -48,27 +48,27 @@ let draw_current_bsp bsp1 bsp2 =
   let rec aux tmp1 tmp2 parite xmin xmax ymin ymax =
     match tmp1,tmp2 with
       L({coord=c;colored=b},g,d),L(l,g',d') ->
-      if parite then
-        begin
-          aux g g' (not parite) xmin c ymin ymax;
-          aux d d' (not parite) c xmax ymin ymax;
-          set_color (line_color tmp2 parite);
-          moveto c ymin;
-          lineto c ymax;
-        end
-      else
-        begin
-          aux g g' (not parite) xmin xmax ymin c;
-          aux d d' (not parite) xmin xmax c ymax;
-          set_color (line_color tmp2 parite);
-          moveto xmin c;
-          lineto xmax c;
-        end
+       if parite then
+         begin
+           aux g g' (not parite) xmin c ymin ymax;
+           aux d d' (not parite) c xmax ymin ymax;
+           set_color (line_color tmp2 parite);
+           moveto c ymin;
+           lineto c ymax;
+         end
+       else
+         begin
+           aux g g' (not parite) xmin xmax ymin c;
+           aux d d' (not parite) xmin xmax c ymax;
+           set_color (line_color tmp2 parite);
+           moveto xmin c;
+           lineto xmax c;
+         end
     | R(Some c),_ ->
-      begin
-        set_color c;
-        fill_rect xmin ymin (xmax-xmin) (ymax-ymin);
-      end
+       begin
+         set_color c;
+         fill_rect xmin ymin (xmax-xmin) (ymax-ymin);
+       end
     | _ -> ()
   in aux bsp1 bsp2 true 0 (size_x()-1) 20 (size_y()-1);
 ;;
@@ -78,24 +78,24 @@ let click_set_color bsp x y=
   let rec aux tmp parite xmin xmax ymin ymax =
     match tmp with
       L({coord=c;colored=b},g,d)->
-      if parite then
-        L({coord=c;colored=b},
-          aux g (not parite) xmin c ymin ymax,
-          aux d (not parite) c xmax ymin ymax)
-      else
-      L({coord=c;colored=b},
-        aux g (not parite) xmin xmax ymin c,
-        aux d (not parite) xmin xmax c ymax)
+       if parite then
+         L({coord=c;colored=b},
+           aux g (not parite) xmin c ymin ymax,
+           aux d (not parite) c xmax ymin ymax)
+       else
+         L({coord=c;colored=b},
+           aux g (not parite) xmin xmax ymin c,
+           aux d (not parite) xmin xmax c ymax)
     | R(Some c)->
-      if xmin<=x && ymin<=y && xmax>=x && ymax>=y then
-        if c = red_rect then R(Some blue_rect)
-        else R(None)
-      else tmp
+       if xmin<=x && ymin<=y && xmax>=x && ymax>=y then
+         if c = red_rect then R(Some blue_rect)
+         else R(None)
+       else tmp
     | R(None) ->
-      if xmin<=x && ymin<=y && xmax>=x && ymax>=y then R(Some red_rect)
-      else tmp
+       if xmin<=x && ymin<=y && xmax>=x && ymax>=y then R(Some red_rect)
+       else tmp
   in aux bsp true 0 (size_x()-1) 20 (size_y()-1)
-
+   
 ;;
 
 let modelisation bsp1 bsp2 =
@@ -104,25 +104,25 @@ let modelisation bsp1 bsp2 =
     match l with
       [] -> []
     | (Some a,f,b)::v ->
-      if a=red_rect then [(true,(f,b))]::(single_clauses v)
-      else [(false,(f,b))]::(single_clauses v)
+       if a=red_rect then [(true,(f,b))]::(single_clauses v)
+       else [(false,(f,b))]::(single_clauses v)
     | (None,f,b)::v -> [(true,(f,b));(false,(f,b))]::(single_clauses v)
   in
   (* liste toutes les combinaisons de litteraux de même polarité possibles*)
   let rec all_clauses l pol=
     match l with
-        [] -> [[]]
-      | [(_,father,leaf)] -> [[(pol,(father,leaf))]]
-      | (_,father,leaf)::v ->
-        let res = List.map (fun x-> (pol,(father,leaf))::x) (all_clauses v pol)
-        in [(pol,(father,leaf))]::(res@(all_clauses v pol))
+      [] -> [[]]
+    | [(_,father,leaf)] -> [[(pol,(father,leaf))]]
+    | (_,father,leaf)::v ->
+       let res = List.map (fun x-> (pol,(father,leaf))::x) (all_clauses v pol)
+       in [(pol,(father,leaf))]::(res@(all_clauses v pol))
   in
-  (* renvoie la CNF=au moins n rectangles rouges parmi m *)
+  (* renvoie la CNF=au moins n rectangles d'une couleur parmi m *)
   let at_least n m l  =
     let x = m-n+1 in
     (List.filter (fun l' -> (List.length l')=x) (all_clauses l true))
   in
-  (* renvoie la CNF=au plus n rectangles rouges parmi m *)
+  (* renvoie la CNF=au plus n rectangles d'une couleur parmi m *)
   let at_most n m l =
     let x = m-(m-n)+1 in
     (List.filter (fun l' -> (List.length l')=x) (all_clauses l false))
@@ -130,21 +130,23 @@ let modelisation bsp1 bsp2 =
   (* renvoie la CNF complete d'une ligne selon sa couleur *)
   let init_fnc c l =
     let m = (List.length l) in
-    let n = if m mod 2 = 0 then m/2 else m/2+1 in
     let k = (single_clauses l) in
-    if c = black then k
-    else if c = red_line then k@(at_least n m l)
-    else if c = blue_line then k@(at_most (n-1) m l)
-    else k@((at_least n m l)@(at_most n m l))
+    let n = m/2 in
+      if c = black then k
+      else if c = red_line then k@(at_least (n+1) m l)
+      else if c = blue_line then
+        if m mod 2 = 0 then k@(at_most (n-1) m l) else k@(at_most n m l)
+      else if m mod 2 = 0 then k@((at_least n m l)@(at_most n m l))
+      else k@((at_least (n+1) m l)@(at_most (n+1) m l))
   in
   let rec aux tmp1 tmp2 parite =
     match tmp1,tmp2 with
       L(l,g,d),L(l',g',d') ->
-      (init_fnc (line_color tmp2 parite) (rectangles_from_line tmp1 parite))
-      @((aux g g' (not parite))@(aux d d' (not parite)))
+       (init_fnc (line_color tmp2 parite) (rectangles_from_line tmp1 parite))
+       @((aux g g' (not parite))@(aux d d' (not parite)))
     | _ -> []
   in aux bsp1 bsp2 true
-
+   
 let extension_to_bsp solution bsp =
   (*colorie les rectangles avec la solution du sat solver*)
   let rec set_rect_color tmp x found=
@@ -153,10 +155,10 @@ let extension_to_bsp solution bsp =
     match tmp with
       R(None) -> if found then R(Some c) else tmp
     | L(l,g,d) ->
-      if found then tmp
-      else if l.coord = father then
-        if leaf then L(l,set_rect_color g x true,d) else L(l,g,set_rect_color d x true)
-      else L(l,set_rect_color g x false,set_rect_color d x false)
+       if found then tmp
+       else if l.coord = father then
+         if leaf then L(l,set_rect_color g x true,d) else L(l,g,set_rect_color d x true)
+       else L(l,set_rect_color g x false,set_rect_color d x false)
     | _ -> tmp
   in
   let rec aux l tmp =
@@ -167,11 +169,11 @@ let extension_to_bsp solution bsp =
 ;;
 
 let main () =
-
+  
   (* Reccueil de données*)
   print_endline "Choisir profondeur maximale bsp";
   let depth = read_int() in
-
+  
   (* Creation partie*)
   let run = ref true and state = ref "play" in
   let jeuFin = ref (random_bsp 700 680 depth) in
@@ -187,104 +189,120 @@ let main () =
   and (u4,v4) = (text_size "Retourner")
   and (u5,v5) = (text_size "Recommencer")
   and (u6,v6) = (text_size "Quitter") in
-
+  
   (* boucle d'interaction *)
   while !run do
-  clear_graph();
-  set_line_width 3;
-  moveto 0 20;
-  set_color black;
-  lineto (size_x()-1) 20;
+    clear_graph();
+    set_line_width 3;
+    moveto 0 20;
+    set_color black;
+    lineto (size_x()-1) 20;
+    
+    match !state with
 
-  match !state with
-  "play" -> (
-  draw_current_bsp !jeuAffiche !jeuFin;
-  synchronize();
-  set_color black;
-  moveto 0 0;
-  draw_string "Solution  Extension";
-  let e = wait_next_event [Button_down] in
-  let x = e.mouse_x and y = e.mouse_y in
-  if x>=0 && x<=u1 && y>=0 && y<=v1 then (
+      (* Affiche la configuration courante, 
+         affiche les boutons Solution et Extension *)
+      "play" -> (
+      draw_current_bsp !jeuAffiche !jeuFin;
+      synchronize();
+      set_color black;
+      moveto 0 0;
+      draw_string "Solution  Extension";
+      (* Evenements click souris *)
+      let e = wait_next_event [Button_down] in
+      let x = e.mouse_x and y = e.mouse_y in
+      if x>=0 && x<=u1 && y>=0 && y<=v1 then (
         jeuAffiche := !jeuFin;
         state := "solution";
-        )
-        else if x>=u1+20 && x<=u1+u2+20 && y>=0 && y<=v2 then (
+      )
+      else if x>=u1+20 && x<=u1+u2+20 && y>=0 && y<=v2 then (
         let model = modelisation !jeuCourant !jeuFin in
         match (Sat.solve model) with
-        Some a -> (state := "available"; extensionList := a)
+          Some a -> (state := "available"; extensionList := a)
         | None -> state := "unavailable"
-        )
+      )
       else (jeuCourant := click_set_color !jeuCourant x y;
-        if (is_full !jeuCourant) && (check_current !jeuCourant !jeuFin)
-        then state := "win" else jeuAffiche := !jeuCourant)
-        )
+            if (is_full !jeuCourant) && (check_current !jeuCourant !jeuFin)
+            then state := "win" else jeuAffiche := !jeuCourant)
+    )
 
-        | "available" -> (
-          set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
-          moveto ((size_x())/10) ((size_y())/2);
-          draw_string "Extension disponible";
-          set_font "-*-fixed-medium-r-semicondensed--20-*-*-*-*-*-iso8859-1";
-          moveto 0 0;
-          draw_string "Continuer Retourner";
-          let e = wait_next_event [Button_down] in
-          let x = e.mouse_x and y = e.mouse_y in
-          if x>=0 && x<=u3 && y>=0 && y<=v3 then (
-            jeuAffiche := extension_to_bsp !extensionList !jeuCourant;
-            state := "solution";
-            )
-            else if x>=u3+20 && x<=u3+u4+20 && y>=0 && y<=v4 then state := "play";
-            )
+    (* Affiche un message indiquant l'existence d'une extension,
+       Affiche les boutons Coutinuer et Retourner *)
+    | "available" -> (
+      set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
+      moveto ((size_x())/10) ((size_y())/2);
+      draw_string "Extension disponible";
+      set_font "-*-fixed-medium-r-semicondensed--20-*-*-*-*-*-iso8859-1";
+      moveto 0 0;
+      draw_string "Continuer Retourner";
+      (* Evenements click souris *)
+      let e = wait_next_event [Button_down] in
+      let x = e.mouse_x and y = e.mouse_y in
+      if x>=0 && x<=u3 && y>=0 && y<=v3 then (
+        jeuAffiche := extension_to_bsp !extensionList !jeuCourant;
+        state := "solution";
+      )
+      else if x>=u3+20 && x<=u3+u4+20 && y>=0 && y<=v4 then state := "play";
+    )
 
-            | "unavailable" -> (
-              set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
-              moveto ((size_x())/10) ((size_y())/2);
-              draw_string "Extension indisponible";
-              set_font "-*-fixed-medium-r-semicondensed--20-*-*-*-*-*-iso8859-1";
-              moveto 0 0;
-              draw_string "Retourner";
-              let e = wait_next_event [Button_down] in
-              let x = e.mouse_x and y = e.mouse_y in
-              if x>=0 && x<=u1 && y>=0 && y<=v1 then state := "play";
-              )
+    (* Affiche un message indiquant l'inexistence d'une extension,
+       Affiche le bouton Retourner *)
+    | "unavailable" -> (
+      set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
+      moveto ((size_x())/10) ((size_y())/2);
+      draw_string "Extension indisponible";
+      set_font "-*-fixed-medium-r-semicondensed--20-*-*-*-*-*-iso8859-1";
+      moveto 0 0;
+      draw_string "Retourner";
+      (* Evenements click souris *)
+      let e = wait_next_event [Button_down] in
+      let x = e.mouse_x and y = e.mouse_y in
+      if x>=0 && x<=u1 && y>=0 && y<=v1 then state := "play";
+    )
 
-              | "solution" -> (
-                draw_current_bsp !jeuAffiche !jeuFin;
-                synchronize();
-                set_color black;
-                moveto 0 0;
-                draw_string "Recommencer Quitter";
-                let e = wait_next_event [Button_down] in
-                let x = e.mouse_x and y = e.mouse_y in
-                if x>=0 && x<=u5 && y>=0 && y<=v5 then (
-                  jeuFin := (random_bsp 700 700 depth);
-                  jeuCourant := (config_initial !jeuFin);
-                  jeuAffiche := !jeuCourant;
-                  state := "play"
-                  )
-                  else if x>=u5+20 && x<=u5+u6+20 && y>=0 && y<= v6 then run := false
-                  )
+    (* Affiche la configuration finale,
+     affiche les boutons Recommencer et Quitter*)
+    | "solution" -> (
+      draw_current_bsp !jeuAffiche !jeuFin;
+      synchronize();
+      set_color black;
+      moveto 0 0;
+      draw_string "Recommencer Quitter";
+      (* Evenements click souris *)
+      let e = wait_next_event [Button_down] in
+      let x = e.mouse_x and y = e.mouse_y in
+      if x>=0 && x<=u5 && y>=0 && y<=v5 then (
+        jeuFin := (random_bsp 700 700 depth);
+        jeuCourant := (config_initial !jeuFin);
+        jeuAffiche := !jeuCourant;
+        state := "play"
+      )
+      else if x>=u5+20 && x<=u5+u6+20 && y>=0 && y<= v6 then run := false
+    )
+                  
+    (* Affiche un message de victoire,
+       Affiche les boutons Recommencer et Quitter *)  
+    | "win" -> (
+      set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
+      moveto ((size_x())/10) ((size_y())/2);
+      draw_string "Felicitations!";
+      set_font "-*-fixed-medium-r-semicondensed--20-*-*-*-*-*-iso8859-1";
+      moveto 0 0;
+      draw_string "Recommencer Quitter";
+      (* Evenements click souris *)
+      let e = wait_next_event [Button_down] in
+      let x = e.mouse_x and y = e.mouse_y in
+      if x>=0 && x<=u5 && y>=0 && y<=v5 then (
+        jeuFin := (random_bsp 700 700 depth);
+        jeuCourant := (config_initial !jeuFin);
+        jeuAffiche := !jeuCourant;
+        state := "play"
+      )
+      else if x>=u5+20 && x<=u5+u6+20 && y>=0 && y<= v6 then run := false
+    )
+    | _ -> ()
+  done;
+;;
 
-                  | "win" -> (
-                    set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
-                    moveto ((size_x())/10) ((size_y())/2);
-                    draw_string "Felicitations!";
-                    set_font "-*-fixed-medium-r-semicondensed--20-*-*-*-*-*-iso8859-1";
-                    moveto 0 0;
-                    draw_string "Recommencer Quitter";
-                    let e = wait_next_event [Button_down] in
-                    let x = e.mouse_x and y = e.mouse_y in
-                    if x>=0 && x<=u5 && y>=0 && y<=v5 then (
-                      jeuFin := (random_bsp 700 700 depth);
-                      jeuCourant := (config_initial !jeuFin);
-                      jeuAffiche := !jeuCourant;
-                      state := "play"
-                      )
-                      else if x>=u5+20 && x<=u5+u6+20 && y>=0 && y<= v6 then run := false
-                      )
-                      | _ -> ()
-                      done;
-                      ;;
-
-                      let _ = main ()
-                      ;;
+let _ = main ()
+;;
